c2240fda23d324664e6b520971b6f029bfd9c3f7
Sumit Mishra <mishrasumeet0707@gmail.com>
2025-07-13 01:48:26 +0530

    star

--- pattern.cpp
+++ pattern.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 using namespace std;
 int main (){
-    for (int i=0; i<4; i++){
-        for (int j=0; j<4; j++){
-            cout<<"*";
+//     for (int i=0; i<4; i++){
+//         for (int j=0; j<4; j++){
+//             cout<<"*";
 
-        }
-        cout<<endl;
-    }
+//         }
+//         cout<<endl;
+//     }
+
+int n ;
+cin>> n;
+
+for (int i=1; i<=n ; i ++){
+    for (int j =1 ; j<=n;j++ ){
+        if (i == n/2+1 || j == n/2+1){
+            cout<<"*";
+        }else( cout<<" ");
+           
+        
+     }
+    cout<<endl;
+}
     return 0;
 }
