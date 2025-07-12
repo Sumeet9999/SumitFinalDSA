b1ba45999955674da9c8d92a445c6b89166394d2
Sumit Mishra <mishrasumeet0707@gmail.com>
2025-07-13 01:23:22 +0530

    crazy

--- oddnumbers.cpp
+++ oddnumbers.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main (){
-    for (int i=0; i<=4; i++){
+    for (int i=1; i<=4; i++){
         int a = 1;
-        for (int j=0; j<=i; j++){
+        for (int j=1; j<=i; j++){
             cout<<a;
             a = a + 2;
 
