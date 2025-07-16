28bbbc76d8c539ae1f261adc5cf3c3fb317802bb
Sumit Mishra <mishrasumeet0707@gmail.com>
2025-07-16 22:45:23 +0530

    array_Sortedcheck

--- checksorting.cpp
+++ checksorting.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 using namespace std;
 int main (){
-    int a[5] = {1, 2, 3, 7, 9};
-    for ( int i = 1; i<5; i++){
-        if (a[i]>= a[i-1]){
-            cout<<"Array Is Sorted";
-        }else{
-            cout<<"Array is not sorted";
+    // int a[5] = {1, 2, 3, 7, 9};
+    // for ( int i = 1; i<5; i++){
+    //     if (a[i]>= a[i-1]){
+    //         cout<<"Array Is Sorted";
+    //     }else{
+    //         cout<<"Array is not sorted";
+    //     }
+    // }
+
+
+
+    // roation 
+    int nums[] = {1, 2, 3, 7, 9};
+        int breakpoints = 0;
+        for ( int i = 0 ; i<5; i++){
+            if (nums[i]>nums[i+1]) breakpoints++;
         }
-    }
-    return 0;
+        if ( nums[4]> nums[0]) breakpoints++;
+        return breakpoints<=1;
+
+    ;
 
 }
+
+
+
+//rotation
