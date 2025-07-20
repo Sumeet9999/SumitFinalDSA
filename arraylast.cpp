c47a9b015f12d0dc1d49900f812e13be966d4fc7
Sumit Mishra <mishrasumeet0707@gmail.com>
2025-07-21 02:16:58 +0530

    day 5 sum 3(optimal)

--- arraylast.cpp
+++ arraylast.cpp
@@ -5,19 +5,29 @@ using namespace std;
 int main() {
     vector<int> nums = {1, 1, 2, 3, 3, 4, 4};
     int n = nums.size();
+    int xorr = 0;
 
-    for (int i = 0; i < n; i++) {
-        int counti = 0;
-        for (int j = 0; j < n; j++) {
-            if (nums[i] == nums[j]) {
-                counti++;
-            }
-        }
-        if (counti == 1) {
-            cout << nums[i] << endl;
-            break; 
-        }
+    // for (int i = 0; i < n; i++) {
+    //     int counti = 0;
+    //     for (int j = 0; j < n; j++) {
+    //         if (nums[i] == nums[j]) {
+    //             counti++;
+    //         }
+    //     }
+    //     if (counti == 1) {
+    //         cout << nums[i] << endl;
+    //         break; 
+    //     }
+    // }
+
+    // optimal 
+    for ( int i = 0 ;i<n ; i++ ){
+        xorr= xorr ^ nums[i];
+        
+
     }
+    cout << xorr; 
+
 
     return 0;
 }
