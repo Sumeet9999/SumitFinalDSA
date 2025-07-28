8833511a97f4cf4800d6dbc89ea215769581b9cb
Sumit Mishra <mishrasumeet0707@gmail.com>
2025-07-29 03:37:03 +0530

    n/2(better)

--- arrays/majorityebytwonby2.cpp
+++ arrays/majorityebytwonby2.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 #include <vector>
+#include<map>
+#include<algorithm>
 using namespace std;
 int main (){
-    vector<int> arr = {2, 2, 3, 3, 1, 2, 2} ;
-    for (int i = 0 ; i< arr.size(); i++ ){
-        int count = 0; 
-        for (int j= 0 ; j< arr.size(); j++ ){
-            if ( arr[i] == arr [j]){
-                count ++ ; 
-            }
-        } 
+    vector<int> arr = {3, 3, 3, 3, 1, 2, 2} ;
+    map<int , int> mpp;
+    // for (int i = 0 ; i< arr.size(); i++ ){
+    //     int count = 0; 
+    //     for (int j= 0 ; j< arr.size(); j++ ){
+    //         if ( arr[i] == arr [j]){
+    //             count ++ ; 
+    //         }
+    //     } 
         
-        if (count > arr.size()/2){
-            cout<<arr[i];
-            break;
-        }   
+    //     if (count > arr.size()/2){
+    //         cout<<arr[i];
+    //         break;
+    //     }   
+    // }
+
+
+    // better using hashing
+    for (int i = 0 ; i<arr.size(); i ++ ){
+        mpp[arr[i]]++;
     }
+    for (auto it : mpp ){
+        if (it.second>(arr.size()/2)){
+            cout<<it.first;
+        }
+    }
+
+
+
 
     return 0 ; 
 
