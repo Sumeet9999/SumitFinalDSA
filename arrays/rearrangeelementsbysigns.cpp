107c223e27e12b11d677af3bded94d6a1d8b3a34
Sumit Mishra <mishrasumeet0707@gmail.com>
2025-08-01 00:58:47 +0530

    rearrnage by signs optimal

--- arrays/rearrangeelementsbysigns.cpp
+++ arrays/rearrangeelementsbysigns.cpp
@@ -5,16 +5,21 @@ using namespace std;
 int main (){
     vector<int> arr = {3, 1, -2, -5, 2, -4 } ;
     int  n = arr.size();
-    vector<int> a (n , 0);
-    int posIndex = 0 , negIndex = 1;
-    for( int i = 0 ; i< arr.size(); i++){
-        if (arr[i]< 0 ){
-            a[negIndex] = arr[i];
-            negIndex +=2;
-        }else{
-            a[posIndex] = arr [i];
-            posIndex+=2;
-        }
-    }
+    // vector<int> a (n , 0);
+    // int posIndex = 0 , negIndex = 1;
+    // for( int i = 0 ; i< arr.size(); i++){
+    //     if (arr[i]< 0 ){
+    //         a[negIndex] = arr[i];
+    //         negIndex +=2;
+    //     }else{
+    //         a[posIndex] = arr [i];
+    //         posIndex+=2;
+    //     }
+    // }
+
+    // optimal 
+
+
+    int postIndex = 0 , negatIndex = 1 ;
     // prinitng not done but logic is correct !
 }    
