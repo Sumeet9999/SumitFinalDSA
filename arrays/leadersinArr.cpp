5dc0edfe0ce27c25df50341bc886445669845c2b
Sumit Mishra <mishrasumeet0707@gmail.com>
2025-08-01 02:08:25 +0530

    leader optimal

--- arrays/leadersinArr.cpp
+++ arrays/leadersinArr.cpp
@@ -5,26 +5,55 @@ using namespace std;
 int main() {
     vector<int> arr = {10, 22, 12, 3, 0, 6};
     vector<int> ans;
+    int n = arr.size();
 
-    for (int i = 0; i < arr.size(); i++) {
-        bool leader = true;
+    // for (int i = 0; i < arr.size(); i++) {
+    //     bool leader = true;
 
-        for (int j = i + 1; j < arr.size(); j++) { // ✅ Correct loop condition
-            if (arr[j] > arr[i]) {
-                leader = false;
-                break;
-            }
-        }
+    //     for (int j = i + 1; j < arr.size(); j++) { // ✅ Correct loop condition
+    //         if (arr[j] > arr[i]) {
+    //             leader = false;
+    //             break;
+    //         }
+    //     }
 
-        if (leader)
+    //     if (leader)
+    //         ans.push_back(arr[i]);
+    // }
+
+    // // Output leaders
+    // for (int i = 0; i < ans.size(); i++) {
+    //     cout << ans[i] << " ";
+    // }
+
+    // cout << endl;
+
+    // optimal 
+
+
+    int maxi = INT_MIN;
+    for ( int i = n-1; i>= 0; i-- ){
+        if (arr[i]> maxi){
             ans.push_back(arr[i]);
+        }
+        maxi = max(maxi, arr[i]);
     }
 
-    // Output leaders
+
+    reverse( ans.begin(), ans.end());
+
     for (int i = 0; i < ans.size(); i++) {
         cout << ans[i] << " ";
     }
 
     cout << endl;
+
+
     return 0;
+
+
+
+
+
 }
+
