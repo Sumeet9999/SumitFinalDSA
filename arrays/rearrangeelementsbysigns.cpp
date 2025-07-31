445a212ee8650cbc8051441e24535e308548a86e
Sumit Mishra <mishrasumeet0707@gmail.com>
2025-07-31 23:58:44 +0530

    rearrnage by signs brute

---
+++ arrays/rearrangeelementsbysigns.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main (){
+    vector<int> arr = {3, 1, -2, -5, 2, -4 } ;
+    int  n = arr.size();
+    vector<int> a (n , 0);
+    int posIndex = 0 , negIndex = 1;
+    for( int i = 0 ; i< arr.size(); i++){
+        if (arr[i]< 0 ){
+            a[negIndex] = arr[i];
+            negIndex +=2;
+        }else{
+            a[posIndex] = arr [i];
+            posIndex+=2;
+        }
+    }
+    // prinitng not done but logic is correct !
+}    
